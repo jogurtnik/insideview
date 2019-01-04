14e4c4b501056393e654beb85692b114a7b97ac0
Remigiusz <remigiusz.widz@gmail.com>
2019-01-04 20:05:40 +0000

    Added QueryFormProcessingManager

---
+++ src/main/java/uk/co/punishell/insideview/model/managers/QueryFormProcessingManager.java
@@ -0,0 +1,23 @@
+package uk.co.punishell.insideview.model.managers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.co.punishell.insideview.model.services.web.forms.QueryFormData;
+import uk.co.punishell.insideview.model.services.web.forms.QueryFormResult;
+
+@Slf4j
+@Component
+public class QueryFormProcessingManager {
+
+    QueryFormData queryFormData;
+    QueryFormResult queryFormResult;
+
+    public QueryFormResult processQueryForm (QueryFormData queryFormData) {
+
+        this.queryFormData = queryFormData;
+
+        // TODO
+
+        return queryFormResult;
+    }
+}
