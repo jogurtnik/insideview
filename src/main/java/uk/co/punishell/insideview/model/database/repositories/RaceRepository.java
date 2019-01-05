06e08fe5615d08ca0f27899981c4c6e44ada897f
Remigiusz <remigiusz.widz@gmail.com>
2019-01-05 18:01:15 +0000

    Created RaceSpecification

--- src/main/java/uk/co/punishell/insideview/model/database/repositories/RaceRepository.java
+++ src/main/java/uk/co/punishell/insideview/model/database/repositories/RaceRepository.java
@@ -1,7 +1,8 @@
 package uk.co.punishell.insideview.model.database.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import uk.co.punishell.insideview.model.database.entities.Race;
 
-public interface RaceRepository extends JpaRepository<Race, Long> {
+public interface RaceRepository extends JpaRepository<Race, Long>, JpaSpecificationExecutor<Race> {
 }
---
+++ src/main/java/uk/co/punishell/insideview/model/database/specifications/RaceSpecification.java
@@ -0,0 +1,28 @@
+package uk.co.punishell.insideview.model.database.specifications;
+
+import org.springframework.data.jpa.domain.Specification;
+import uk.co.punishell.insideview.model.database.entities.Race;
+import uk.co.punishell.insideview.model.services.web.forms.QueryFormData;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RaceSpecification implements Specification<Race> {
+
+    QueryFormData criteria;
+
+    @Override
+    public Predicate toPredicate(Root<Race> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+
+        final List<Predicate> predicates = new ArrayList<>();
+
+        // TODO
+
+        return null;
+    }
+
+}
