2e9fae129944e7edcc3c6149b626dff59cdf3d66
jogurtnik <remigiusz.widz@gmail.com>
2019-03-10 14:32:37 +0000

    Refactored datatables initialization and custom cell formatting

---
+++ src/main/resources/static/js/datatables/custom.dataTables.styles.js
@@ -0,0 +1,129 @@
+$(document).ready(function() {
+
+    init_DataTables();
+
+});
+
+function init_DataTables() {
+
+    let domCommand = 'Blfrtip';
+    let buttonsMap = [
+        {
+            extend: "copy",
+            className: "btn-sm"
+        },
+        {
+            extend: "csv",
+            className: "btn-sm"
+        },
+        {
+            extend: "excel",
+            className: "btn-sm"
+        },
+        {
+            extend: "pdfHtml5",
+            className: "btn-sm"
+        },
+        {
+            extend: "print",
+            className: "btn-sm"
+        },
+    ];
+
+    $('.dataTableRace').DataTable({
+
+        dom: domCommand,
+        buttons: buttonsMap,
+
+        rowCallback: cellsColorFormatting
+
+    });
+
+    $('#dataTableRunners').DataTable({
+        "ordering": false,
+        dom: domCommand,
+        buttons: buttonsMap,
+        rowCallback: cellsColorFormatting
+    });
+
+    function cellsColorFormatting(row, data) {
+
+        // conditional background-color formatting for HorseName column
+        if (data[7] == 'Won') {
+            $(row).find('td:eq(0)').css('background-color', '#3ED578');
+            $(row).find('td:eq(7)').css('background-color', '#3ED578');
+        } else if (data[7] == 'Placed') {
+            $(row).find('td:eq(0)').css('background-color', '#f2dac1');
+            $(row).find('td:eq(7)').css('background-color', '#f2dac1');
+        }
+
+        // conditional background-color formatting for MovAM column
+        if (data[2] > 5) {
+            $(row).find('td:eq(2)').css('background-color', '#99ccff');
+
+        } else if (data[2] >= 2.5 && data[2] <= 5) {
+            $(row).find('td:eq(2)').css('background-color', '#ccffcc');
+
+        } else if (data[2] > 0 && data[2] < 2.5) {
+            $(row).find('td:eq(2)').css('background-color', '#ffff99');
+
+        } else if (data[2] < 0 && data[2] > -2.5) {
+            $(row).find('td:eq(2)').css('background-color', '#ffcc99');
+
+        } else if (data[2] <= -2.5) {
+            $(row).find('td:eq(2)').css('background-color', '#ff99cc');
+        }
+
+        // conditional background-color formatting for Mov1 column
+        if (data[4] > 5) {
+            $(row).find('td:eq(4)').css('background-color', '#99ccff');
+
+        } else if (data[4] >= 2.5 && data[4] <= 5) {
+            $(row).find('td:eq(4)').css('background-color', '#ccffcc');
+
+        } else if (data[4] > 0 && data[4] < 2.5) {
+            $(row).find('td:eq(4)').css('background-color', '#ffff99');
+
+        } else if (data[4] < 0 && data[4] > -2.5) {
+            $(row).find('td:eq(4)').css('background-color', '#ffcc99');
+
+        } else if (data[4] <= -2.5) {
+            $(row).find('td:eq(4)').css('background-color', '#ff99cc');
+        }
+
+        // conditional background-color formatting for Mean column
+        if (data[5] > 5) {
+            $(row).find('td:eq(5)').css('background-color', '#99ccff');
+
+        } else if (data[5] >= 2.5 && data[5] <= 5) {
+            $(row).find('td:eq(5)').css('background-color', '#ccffcc');
+
+        } else if (data[5] > 0 && data[5] < 2.5) {
+            $(row).find('td:eq(5)').css('background-color', '#ffff99');
+
+        } else if (data[5] < 0 && data[5] > -2.5) {
+            $(row).find('td:eq(5)').css('background-color', '#ffcc99');
+
+        } else if (data[5] <= -2.5) {
+            $(row).find('td:eq(5)').css('background-color', '#ff99cc');
+        }
+
+        // conditional background-color formatting for 321 column
+        if (data[6] > 5) {
+            $(row).find('td:eq(6)').css('background-color', '#99ccff');
+
+        } else if (data[6] >= 2.5 && data[6] <= 5) {
+            $(row).find('td:eq(6)').css('background-color', '#ccffcc');
+
+        } else if (data[6] > 0 && data[6] < 2.5) {
+            $(row).find('td:eq(6)').css('background-color', '#ffff99');
+
+        } else if (data[6] < 0 && data[6] > -2.5) {
+            $(row).find('td:eq(6)').css('background-color', '#ffcc99');
+
+        } else if (data[6] <= -2.5) {
+            $(row).find('td:eq(6)').css('background-color', '#ff99cc');
+        }
+    }
+
+}
