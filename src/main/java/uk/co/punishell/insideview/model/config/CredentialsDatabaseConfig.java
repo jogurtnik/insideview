ee0ead9f4166c0244d56a98eb6335bf00f0e3e3b
jogurtnik <remigiusz.widz@gmail.com>
2019-03-11 22:54:03 +0000

    JDBC Authentication - application properties - DataSource Bean - updated AuthenticationManagerBuilder

---
+++ src/main/java/uk/co/punishell/insideview/model/config/CredentialsDatabaseConfig.java
@@ -0,0 +1,48 @@
+package uk.co.punishell.insideview.model.config;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
+@Slf4j
+@Configuration
+@PropertySource("classpath:persistence-mysql.properties")
+public class CredentialsDatabaseConfig {
+
+    @Autowired
+    Environment env;
+
+    @Bean
+    public DataSource securityDataSource() {
+
+        // create connection pool
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+        // set the jdbc driver
+        try {
+            securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException exc) {
+            throw new RuntimeException(exc);
+        }
+
+        // set database connection props
+        securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        securityDataSource.setUser(env.getProperty("jdbc.user"));
+        securityDataSource.setPassword(env.getProperty("jdbc.password"));
+
+        // set connection pool props
+        securityDataSource.setInitialPoolSize(Integer.parseInt(env.getProperty("connection.pool.initialPoolSize")));
+        securityDataSource.setMinPoolSize(Integer.parseInt(env.getProperty("connection.pool.minPoolSize")));
+        securityDataSource.setMaxPoolSize(Integer.parseInt(env.getProperty("connection.pool.maxPoolSize")));
+        securityDataSource.setMaxIdleTime(Integer.parseInt(env.getProperty("connection.pool.maxIdleTime")));
+
+        return securityDataSource;
+    }
+}
--- src/main/java/uk/co/punishell/insideview/model/config/WebSecurityConfig.java
+++ src/main/java/uk/co/punishell/insideview/model/config/WebSecurityConfig.java
@@ -1,16 +1,21 @@
 package uk.co.punishell.insideview.model.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource securityDataSource;
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
@@ -38,10 +43,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
 
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        auth.jdbcAuthentication().dataSource(securityDataSource);
 
-        auth.inMemoryAuthentication()
-                .withUser(users.username("admin").password("shitronica").roles("USER", "ADMIN"));
     }
 
 }
