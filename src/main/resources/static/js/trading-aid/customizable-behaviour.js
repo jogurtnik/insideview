e52f1728d1aa6d812783ab166d24e55c7139d42a
jogurtnik <remigiusz.widz@gmail.com>
2019-04-20 18:45:13 +0000

    Added saving nav menu class in the cookies

--- src/main/resources/static/js/trading-aid/customizable-behaviour.js
+++ src/main/resources/static/js/trading-aid/customizable-behaviour.js
@@ -6,13 +6,16 @@ $(document).ready(function() {
     $('.saveScrollPosition').on("click", function (e) {
         e.preventDefault();
         let currentYOffset = window.pageYOffset;  // save current page postion.
-        Cookies.set('jumpToScrollPostion', currentYOffset);
+        Cookies.set('jumpToScrollPosition', currentYOffset);
 
         let raceTable = document.getElementById("raceTable").style.display;
         let raceTableMin = document.getElementById("raceTableMin").style.display;
 
         Cookies.set('raceTableStatus', raceTable);
         Cookies.set('raceTableMinStatus', raceTableMin);
+
+        let body = document.getElementsByTagName('body')[0];
+        Cookies.set('bodyClass', body.className);
 
         if(!$(this).attr("data-confirm")) {  // if there is no data-confirm on this link then trigger the click. else we have issues.
             let url = this.href;
@@ -22,17 +25,38 @@ $(document).ready(function() {
         }
     });
 
-    // check if we should jump to position.
-    if(Cookies.get('jumpToScrollPostion') !== "undefined") {
+    // check if is saved body class to define expanded or collapsed navigation menu
+    if (Cookies.get('bodyClass').toString() !== "undefined") {
+
+        let body = document.getElementsByTagName('body')[0];
+        let bodyClass = Cookies.get('bodyClass');
+        $(body).removeClass("nav-md");
+        $(body).addClass(bodyClass);
+        Cookies.remove('bodyClass');
+    }
+
+    // check if raceTable should be hidden or shown
+    if (Cookies.get('raceTableStatus') !== "undefined") {
 
         let raceTable = document.getElementById("raceTable");
-        let raceTableMin = document.getElementById("raceTableMin");
-
         raceTable.style.display = Cookies.get('raceTableStatus');
-        raceTableMin.style.display = Cookies.get('raceTableMinStatus');
+        Cookies.remove('raceTableStatus');
+    }
 
-        let jumpTo = Cookies.get('jumpToScrollPostion');
+    // check if compact raceTable should be hidden or shown
+    if (Cookies.get('raceTableMinStatus') !== "undefined") {
+
+        let raceTableMin = document.getElementById("raceTableMin");
+        raceTableMin.style.display = Cookies.get('raceTableMinStatus');
+        Cookies.remove('raceTableMinStatus');
+    }
+
+    // check if we should jump to position.
+    if(Cookies.get('jumpToScrollPosition') !== "undefined") {
+
+
+        let jumpTo = Cookies.get('jumpToScrollPosition');
         window.scrollTo(0, jumpTo);
-        Cookies.remove('jumpToScrollPostion');  // and delete cookie so we don't jump again.
+        Cookies.remove('jumpToScrollPosition');  // and delete cookie so we don't jump again.
     }
 });
