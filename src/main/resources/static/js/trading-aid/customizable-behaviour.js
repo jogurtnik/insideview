aa5642f68dfe2f16ceb0848d265355b80a7f1c82
jogurtnik <remigiusz.widz@gmail.com>
2019-06-07 22:24:28 +0000

    Added refresh every 60 seconds to Trading Aid page

--- src/main/resources/static/js/trading-aid/customizable-behaviour.js
+++ src/main/resources/static/js/trading-aid/customizable-behaviour.js
@@ -2,9 +2,9 @@
 
 $(document).ready(function() {
 
-    // on certain links save the scroll postion.
-    $('.saveScrollPosition').on("click", function (e) {
-        e.preventDefault();
+    // define refresh function
+    let refresh = function() {
+
         let currentYOffset = window.pageYOffset;  // save current page postion.
         Cookies.set('jumpToScrollPosition', currentYOffset);
 
@@ -17,46 +17,51 @@ $(document).ready(function() {
         let body = document.getElementsByTagName('body')[0];
         Cookies.set('bodyClass', body.className);
 
-        if(!$(this).attr("data-confirm")) {  // if there is no data-confirm on this link then trigger the click. else we have issues.
-            let url = this.href;
-            window.location = url;
+        if (!$(this).attr("data-confirm")) {  // if there is no data-confirm on this link then trigger the click. else we have issues.
+            window.location = this.href;
             //$(this).trigger('click');  // continue with click event.
             location.reload(true);
         }
+    };
+
+        // on certain links save the scroll postion.
+        $('.saveScrollPosition').on("click", refresh);
+
+        // check if is saved body class to define expanded or collapsed navigation menu
+        if (Cookies.get('bodyClass') !== "undefined") {
+
+            let body = document.getElementsByTagName('body')[0];
+            let bodyClass = Cookies.get('bodyClass');
+            $(body).removeClass("nav-md");
+            $(body).addClass(bodyClass);
+            Cookies.remove('bodyClass');
+        }
+
+        // check if raceTable should be hidden or shown
+        if (Cookies.get('raceTableStatus') !== "undefined") {
+
+            let raceTable = document.getElementById("raceTable");
+            raceTable.style.display = Cookies.get('raceTableStatus');
+            Cookies.remove('raceTableStatus');
+        }
+
+        // check if compact raceTable should be hidden or shown
+        if (Cookies.get('raceTableMinStatus') !== "undefined") {
+
+            let raceTableMin = document.getElementById("raceTableMin");
+            raceTableMin.style.display = Cookies.get('raceTableMinStatus');
+            Cookies.remove('raceTableMinStatus');
+        }
+
+        // check if we should jump to position.
+        if(Cookies.get('jumpToScrollPosition') !== "undefined") {
+
+
+            let jumpTo = Cookies.get('jumpToScrollPosition');
+            window.scrollTo(0, jumpTo);
+            Cookies.remove('jumpToScrollPosition');  // and delete cookie so we don't jump again.
+        }
+
+        // refresh the web page every 60 seconds
+        setTimeout(refresh, 60000);
     });
-
-    // check if is saved body class to define expanded or collapsed navigation menu
-    if (Cookies.get('bodyClass').toString() !== "undefined") {
-
-        let body = document.getElementsByTagName('body')[0];
-        let bodyClass = Cookies.get('bodyClass');
-        $(body).removeClass("nav-md");
-        $(body).addClass(bodyClass);
-        Cookies.remove('bodyClass');
-    }
-
-    // check if raceTable should be hidden or shown
-    if (Cookies.get('raceTableStatus') !== "undefined") {
-
-        let raceTable = document.getElementById("raceTable");
-        raceTable.style.display = Cookies.get('raceTableStatus');
-        Cookies.remove('raceTableStatus');
-    }
-
-    // check if compact raceTable should be hidden or shown
-    if (Cookies.get('raceTableMinStatus') !== "undefined") {
-
-        let raceTableMin = document.getElementById("raceTableMin");
-        raceTableMin.style.display = Cookies.get('raceTableMinStatus');
-        Cookies.remove('raceTableMinStatus');
-    }
-
-    // check if we should jump to position.
-    if(Cookies.get('jumpToScrollPosition') !== "undefined") {
-
-
-        let jumpTo = Cookies.get('jumpToScrollPosition');
-        window.scrollTo(0, jumpTo);
-        Cookies.remove('jumpToScrollPosition');  // and delete cookie so we don't jump again.
-    }
-});
