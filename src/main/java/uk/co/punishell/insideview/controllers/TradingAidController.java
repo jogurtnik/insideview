45705f8c918fc8c76a9aaca7bda7d7f1590d09e6
jogurtnik <remigiusz.widz@gmail.com>
2019-08-13 19:01:03 +0100

    Added export to excel button to Trading Aid page Partially implemented Controller method

--- src/main/java/uk/co/punishell/insideview/controllers/TradingAidController.java
+++ src/main/java/uk/co/punishell/insideview/controllers/TradingAidController.java
@@ -1,6 +1,8 @@
 package uk.co.punishell.insideview.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -8,14 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import uk.co.punishell.insideview.model.exceptions.VendorsException;
 import uk.co.punishell.insideview.model.services.JSHDataScrapper.JSHRaceAssembler;
 import uk.co.punishell.insideview.model.services.JSHDataScrapper.JSHTradingAidAssembler;
 import uk.co.punishell.insideview.model.services.JSHDataScrapper.WebpageScrapper;
 import uk.co.punishell.insideview.view.commands.entityCommands.JSHRaceCommand;
-import uk.co.punishell.insideview.view.commands.guiCommands.TraidingAidCommand;
+import uk.co.punishell.insideview.view.commands.guiCommands.TradingAidCommand;
 
-import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -58,7 +64,7 @@ public class TradingAidController {
 
         log.debug("Loaded Races: " + raceInfo.size());
 
-        TraidingAidCommand tradingAidCommand = new TraidingAidCommand();
+        TradingAidCommand tradingAidCommand = new TradingAidCommand();
 
         if (raceInfo != null &&
             raceInfo.size() > 0 &&
@@ -92,5 +98,85 @@ public class TradingAidController {
         model.addAttribute("jshData", tradingAidCommand);
 
         return "tradingAid";
+    }
+
+    // TODO add custom exception
+    @RequestMapping("exportToExcel")
+    public void exportFundamentalsToExcel(@ModelAttribute("jshData") TradingAidCommand jshData,
+                                            HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        // create a new file
+        FileOutputStream outExcel = new FileOutputStream("workbook.xls");
+        // create a new workbook
+        Workbook workbook = new HSSFWorkbook();
+        // create a new sheet
+        Sheet sheet = workbook.createSheet();
+
+        sheet.setDefaultColumnWidth(7);
+        /*sheet.setColumnWidth(0, 20);
+        sheet.setColumnWidth(14, 20);
+        sheet.setColumnWidth(21, 20);*/
+
+        // create style for header cells
+        CellStyle style = workbook.createCellStyle();
+        Font font = workbook.createFont();
+        font.setFontName("Arial");
+        font.setBold(true);
+        style.setFont(font);
+
+        // TODO set the cells conditional color formatting
+
+        // create header row
+        Row header = sheet.createRow(0);
+        header.createCell(0).setCellValue("Horse");
+        header.createCell(1).setCellValue("9am");
+        header.createCell(2).setCellValue("MovAM");
+        header.createCell(3).setCellValue("60min");
+        header.createCell(4).setCellValue("Mov60");
+        header.createCell(5).setCellValue("1min");
+        header.createCell(6).setCellValue("Mov1");
+        header.createCell(7).setCellValue("Mean");
+        header.createCell(8).setCellValue("321");
+        header.createCell(9).setCellValue("Result");
+        header.createCell(10).setCellValue("CPR");
+        header.createCell(11).setCellValue("NPTips");
+        header.createCell(12).setCellValue("Naps");
+        header.createCell(13).setCellValue("Stars");
+        header.createCell(14).setCellValue("Jockey");
+        header.createCell(15).setCellValue("Wins");
+        header.createCell(16).setCellValue("R");
+        header.createCell(17).setCellValue("Rs");
+        header.createCell(18).setCellValue("Mov9-60");
+        header.createCell(19).setCellValue("FP");
+        header.createCell(20).setCellValue("HG");
+        header.createCell(21).setCellValue("Trainer");
+        header.createCell(22).setCellValue("Wins");
+        header.createCell(23).setCellValue("R");
+        header.createCell(24).setCellValue("Rs");
+
+        // TODO Write the data
+
+        sheet.autoSizeColumn(0);
+        sheet.autoSizeColumn(14);
+        sheet.autoSizeColumn(21);
+
+        header.setRowStyle(style);
+
+        workbook.write(outExcel);
+
+        response.setContentType("application/octet-stream");
+
+        FileInputStream inputStream = new FileInputStream(new File("workbook.xls"));
+
+        OutputStream outputStream = response.getOutputStream();
+
+        byte[] buffer = new byte[1024];
+        int bytesRead = -1;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {  // write bytes read from the input stream into the output stream
+            outputStream.write(buffer, 0, bytesRead);
+        }
+
+        outputStream.flush();
+
     }
 }
--- src/main/java/uk/co/punishell/insideview/view/commands/guiCommands/TradingAidCommand.java
+++ src/main/java/uk/co/punishell/insideview/view/commands/guiCommands/TradingAidCommand.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Setter
 @Getter
 @NoArgsConstructor
-public class TraidingAidCommand {
+public class TradingAidCommand {
 
     private List<JSHRaceCommand> races = new ArrayList<>();
     private String errorMessage;
